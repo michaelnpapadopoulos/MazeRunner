4ddd8fe68a50a199adcb8a58aeee0758944fc3ab
Maiku <91214380+Maikulol@users.noreply.github.com>
2025-01-19 17:49:49 -0500

    Initial commit, setting up repo and basics

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,16 +5,27 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
-
+    
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+
+        Options options = new Options();
+        options.addOption("i", true, "Path to the input maze txt file");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            cmd = parser.parse(options, args);
+            String mazePath = cmd.getOptionValue("i");
+            
+            logger.trace("**** Reading the maze from file {}", mazePath);
+            BufferedReader reader = new BufferedReader(new FileReader(mazePath));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -26,11 +37,13 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
+            reader.close();
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\\\ An error has occured /!\\\\");
         }
-        System.out.println("**** Computing path");
+
+        logger.trace("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.info("** End of MazeRunner");
     }
 }
